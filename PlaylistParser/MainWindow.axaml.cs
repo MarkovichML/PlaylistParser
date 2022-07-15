c18f8a391df26fb47f23048a3da6654a39b48be3
MykhailoSalivonov <smisha87@gmail.com>
2022-07-15 21:41:05 +0300

    Add project files.

---
+++ PlaylistParser/MainWindow.axaml.cs
@@ -0,0 +1,73 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Collections.Generic;
+using Avalonia.Collections;
+
+namespace PlaylistParser
+{
+    public partial class MainWindow : Window
+    {
+        public string imagePath = "Image/AlbumAvatar.jpg";
+        public MainWindow()
+        {
+            InitializeComponent();
+            var startButton = this.Get<Button>("parseUrlButton");
+            startButton.Click += startButton_Click;
+
+        }
+
+        private void startButton_Click(object? sender, RoutedEventArgs e)
+        {
+            //HtmlHelper htmlHelper = new HtmlHelper();
+            //string url = urlForParseBox.Text;
+            //songs = htmlHelper.GetSongsInformation(url);
+            //collectionView = new DataGridCollectionView(songs);
+            //songsDataGrid.Items = collectionView;
+            PlaylistInformation playlistPresentation = new PlaylistInformation();
+            playlistPresentation.PlaylistName = "ПЛЕЙЛИСТ ДЛЯ ЗНИЩЕННЯ ОРКІВ";
+            albumName.Text = playlistPresentation.PlaylistName;
+            playlistPresentation.Description = $"НАША РУСОФОБІЯ НЕДОСТАТНЯ, ПІДВИЩИТИ ЇЇ МОЖНА ПОСЛУХАВШИ {playlistPresentation.PlaylistName}";
+            descriptionOfAlbum.Text = playlistPresentation.Description;
+            playlistPresentation.SongInfo = UpdateDataGrid();
+            DataGridCollectionView dg = new DataGridCollectionView(playlistPresentation.SongInfo);
+            songsDataGrid.Items = dg;
+            AlbumAvatar.IsVisible = true;
+        }
+    
+        public List<SongInformation> UpdateDataGrid()
+        {
+            List<SongInformation> songs = new List<SongInformation>()
+            {
+                new SongInformation()
+                {
+                    SongName = "Батько наш Бандера",
+                    ArtistName = "Олег Луцан",
+                    AlbumName = "Файний альбом",
+                    Duration = "2:41"
+                },
+                new SongInformation()
+                {
+                    SongName = "Гімп УПА",
+                    ArtistName = "Тінь Сонця",
+                    AlbumName = "Мальовничий альбом",
+                    Duration = "2:40"
+                },
+                new SongInformation()
+                {
+                    SongName = "Сон",
+                    ArtistName = "Степан Гіга",
+                    AlbumName = "Романтичний альбом",
+                    Duration = "2:49"
+                },
+                new SongInformation()
+                {
+                    SongName = "В мене немає дому",
+                    ArtistName = "Один в каное",
+                    AlbumName = "Драматичний альбом",
+                    Duration = "2:41"
+                },
+            };
+            return songs;
+        }
+    }
+}
---
+++ PlaylistParser/PlaylistInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+
+namespace PlaylistParser
+{
+    class PlaylistInformation
+    {
+        public Image PlayListAvatar { get; set; }
+        public string Description { get; set; }
+        public List<SongInformation> SongInfo { get; set; }
+        public string PlaylistName { get; set; }
+    }
+}
